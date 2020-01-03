83236161866b0e59fec074b289870022f5f7460f
EC2 Default User <ec2-user@ip-172-31-22-17.ap-northeast-1.compute.internal>
2020-01-03 01:17:35 +0000

    opencv and gql

--- source/lambda_function.py
+++ source/lambda_function.py
@@ -1,18 +1,31 @@
 # coding: UTF-8
 import boto3
 import os
-import re
-import json
-import datetime
 from urllib.parse import unquote_plus
-
 import numpy as np
 import cv2
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 s3 = boto3.client("s3")
+
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+ENDPOINT = "https://**********.appsync-api.ap-northeast-1.amazonaws.com/graphql"
+API_KEY = "da2-**********"
+_headers = {
+    "Content-Type": "application/graphql",
+    "x-api-key": API_KEY,
+}
+_transport = RequestsHTTPTransport(
+    headers = _headers,
+    url = ENDPOINT,
+    use_json = True,
+)
+_client = Client(
+    transport = _transport,
+    fetch_schema_from_transport = True,
+)
 
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
@@ -20,32 +33,56 @@ def lambda_handler(event, context):
     logger.info("Function Start (deploy from S3) : Bucket={0}, Key={1}" .format(bucket, key))
 
     fileName = os.path.basename(key)
-    localTmpPath = u'/tmp/' + fileName
-    localTmpPath2 = u'/tmp2/' + fileName
+    dirPath = os.path.dirname(key)
+    dirName = os.path.basename(dirPath)
+    
+    orgFilePath = u'/tmp/' + fileName
+    processedFilePath = u'/tmp/processed-' + fileName
 
-    if (not key.startswith("public")):
+    if (key.startswith("public/processed/")):
         logger.info("not start with public")
         return
+    
+    apiCreateTable(dirName, key)
 
-    keyOut = key.replace("public", "processed", 1)
-    logger.info("Output key = {0}".format(keyOut))
+    keyOut = key.replace("public", "public/processed", 1)
+    logger.info("Output local path = {0}".format(processedFilePath))
 
     try:
-        s3.download_file(Bucket=bucket, Key=key, Filename=localTmpPath)
-        
-        before = cv2.imread(localTmpPath)
-        gray = cv2.cvtColor(before, cv2.COLOR_RGB2GRAY)
-        
-        cv2.imwrite(localTmpPath2, gray)
-        s3.upload_file(Filename=localTmpPath2, Bucket=bucket, Key=keyOut)
+        s3.download_file(Bucket=bucket, Key=key, Filename=orgFilePath)
 
-        logger.info("process completed !!")
+        orgImage = cv2.imread(orgFilePath)
+        grayImage = cv2.cvtColor(orgImage, cv2.COLOR_RGB2GRAY)
+        cv2.imwrite(processedFilePath, grayImage)
+
+        s3.upload_file(Filename=processedFilePath, Bucket=bucket, Key=keyOut)
+        apiCreateTable(dirName, keyOut)
+
+        logger.info("Function Completed : processed key = {0}".format(keyOut))
+
     except Exception as e:
         print(e)
         raise e
         
     finally:
-        if os.path.exists(localTmpPath):
-            os.remove(localTmpPath)
-        if os.path.exists(localTmpPath2):
-            os.remove(localTmpPath2)
+        if os.path.exists(orgFilePath):
+            os.remove(orgFilePath)
+        if os.path.exists(processedFilePath):
+            os.remove(processedFilePath)
+
+def apiCreateTable(group, path):
+    logger.info("group={0}, path={1}".format(group, path))
+    try:
+        query = gql("""
+            mutation create {{
+                createSampleAppsyncTable(input:{{
+                group: \"{0}\"
+                path: \"{1}\"
+              }}){{
+                group path
+              }}
+            }}
+            """.format(group, path))
+        _client.execute(query)
+    except Exception as e:
+        print(e)
